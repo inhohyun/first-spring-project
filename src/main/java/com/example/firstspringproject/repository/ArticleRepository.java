62c7226fd033f84b3d892ab6a0032f4e2333841d
인호현 <96523102+inhohyun@users.noreply.github.com>
2023-09-06 19:42:34 +0900

    웹 브라우저에 모든 데이터 출력하기

--- src/main/java/com/example/firstspringproject/repository/ArticleRepository.java
+++ src/main/java/com/example/firstspringproject/repository/ArticleRepository.java
@@ -3,9 +3,14 @@ package com.example.firstspringproject.repository;
 import com.example.firstspringproject.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 //CrudRepository를 상속받아 Repository 인터페이스 구성하기
 public interface ArticleRepository extends CrudRepository<Article, Long> {
 
+    @Override
+    ArrayList<Article> findAll();
 
 }
